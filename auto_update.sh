c1510a13be635cd0ee3cb9376d3824dfe45e9926
Joe.Huang.J <joe.huang.j00@gmail.com>
2026-02-26 15:24:21 +0800

    update auto_update shell script

--- auto_update.sh
+++ auto_update.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+log () {
+    echo -e "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
+log "Session-1: add file"
 git add .
+log "Session-2: Commit"
 git commit -m "$1"
+log "Session-3: Push to Origin Reopsitry"
 git push origin main
